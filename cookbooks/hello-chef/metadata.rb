09f2df8f9d806c8598c45dbdd7077b7bd03e5221
志水友輔 <y3-shimizu@shimizutomotasukunoMacBook-ea.local>
2018-11-17 17:22:10 +0900

    modify chef_version

--- cookbooks/hello-chef/metadata.rb
+++ cookbooks/hello-chef/metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures hello-chef'
 long_description 'Installs/Configures hello-chef'
 version '0.1.0'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 12.9.41' if respond_to?(:chef_version)
 source_url 'https://github.com/Yusuke-Shimizu/circleci-test'
 issues_url "#{source_url}/issues"
 supports "ubuntu", '>= 18.04'
